00075705fc96acf5ef8df5661ce1686d2080f404
CrisSotomayor <cristina@sotomayor.com.mx>
2020-06-07 20:42:48 -0500

    Added thresholds to run multiprocessing by mutation

--- parallel_getdata.py
+++ parallel_getdata.py
@@ -68,53 +68,78 @@ def GetData(tuple):
     return
 
 if __name__ == '__main__':
+
+    thresholds = [round(i, 1) for i in np.linspace(3, 10, 71)]
+    proteins = ["1nd4", "3dqw", "3s4y", "6r5k"]
+
+    home_path = os.path.join(os.getenv("HOME"), "perturbation_networks")
+
     with mp.Manager() as manager:
+        for protein in proteins:
+            # Path where all necessary pdbs are stored
+            path = os.path.join(home_path, f"{protein}")
+            # Path were resultings data csvs will be stored
+            csv_path = os.path.join(home_path, "data")
+            # Dir in path for protein
+            prot_csv_path = os.path.join(csv_path, f"{protein}")
+            # Create paths if they don't exist
+            if not os.path.exists(csv_path):
+                os.makedirs(csv_path)
+            if not os.path.exists(prot_csv_path):
+                os.makedirs(prot_csv_path)
+            # Original data matrix
+            original_data = np.zeros((4, len(thresholds)))
 
-        # original_data matrix missing here, add when iterating over thresholds
+            AA = manager.list(list(Bio.PDB.Polypeptide.aa1)) # List of amino acids
+            mutations_positions = it.MutationsList(path, protein)
+            mutations = manager.list(mutations_positions[0])
+            positions = manager.list(mutations_positions[1])
+            N = len(AA) # Number of amino acids
+            M = len(positions) # Number of mutated positions
 
-        # Path where all necessary pdbs are stored
-        path = "/Users/macbook/Documents/perturbation-networks/proteins/parallel"
-        # Path were resultings data csvs will be stored
-        csv_path = "/Users/macbook/Documents/perturbation-networks/proteins/data"
-        protein = "1nd4"
-        threshold = 5
+            for threshold in thresholds:
+                # Create arrays to store data
+                # https://jonasteuwen.github.io/numpy/python/multiprocessing/2017/01/07/multiprocessing-numpy-array.html
+                nodes1 = np.ctypeslib.as_ctypes(np.zeros((N, M)))
+                edges1 = np.ctypeslib.as_ctypes(np.zeros((N, M)))
+                weight1 = np.ctypeslib.as_ctypes(np.zeros((N, M)))
+                distance1 = np.ctypeslib.as_ctypes(np.zeros((N, M)))
+                # Shared arrays
+                nodes =  sharedctypes.RawArray(nodes1._type_, nodes1)
+                edges =  sharedctypes.RawArray(edges1._type_, edges1)
+                weight =  sharedctypes.RawArray(weight1._type_, weight1)
+                distance =  sharedctypes.RawArray(distance1._type_, distance1)
 
-        AA = manager.list(list(Bio.PDB.Polypeptide.aa1)) # List of amino acids
-        mutations_positions = it.MutationsList(path, protein)
-        mutations = manager.list(mutations_positions[0])
-        positions = manager.list(mutations_positions[1])
-        N = len(AA) # Number of amino acids
-        M = len(positions) # Number of mutated positions
+                # Generate molecule of original pdb file
+                original_prot = it.Pmolecule(os.path.join(path, f"{protein}.pdb"))
+                original = original_prot.network(cutoff=threshold)
+                matrix = np.ctypeslib.as_ctypes(nx.adjacency_matrix(original).toarray())
+                # Shared data for original matrix with threshold
+                original_matrix = sharedctypes.RawArray(matrix._type_, matrix)
 
-        # Create arrays to store data
-        # https://jonasteuwen.github.io/numpy/python/multiprocessing/2017/01/07/multiprocessing-numpy-array.html
-        nodes1 = np.ctypeslib.as_ctypes(np.zeros((N, M)))
-        edges1 = np.ctypeslib.as_ctypes(np.zeros((N, M)))
-        weight1 = np.ctypeslib.as_ctypes(np.zeros((N, M)))
-        distance1 = np.ctypeslib.as_ctypes(np.zeros((N, M)))
-        #shared arrays
-        nodes =  sharedctypes.RawArray(nodes1._type_, nodes1)
-        edges =  sharedctypes.RawArray(edges1._type_, edges1)
-        weight =  sharedctypes.RawArray(weight1._type_, weight1)
-        distance =  sharedctypes.RawArray(distance1._type_, distance1)
+                # Save original data
+                original_data[0][i] = it.GetNodes(original)
+                original_data[1][i] = it.GetEdges(original)
+                original_data[2][i] = it.GetWeight(original)
+                original_data[3][i] = it.GetDistance(original)
 
-        # save original data here
+                # Get list of tuples to map in Pool
+                tuples = [(protein, mutation, path, threshold) for mutation in mutations]
 
-        # Generate molecule of original pdb file
-        original_prot = it.Pmolecule(os.path.join(path, f"{protein}.pdb"))
-        original = original_prot.network(cutoff=threshold)
-        matrix = np.ctypeslib.as_ctypes(nx.adjacency_matrix(original).toarray())
-        original_matrix = sharedctypes.RawArray(matrix._type_, matrix)
+                with mp.Pool(30) as pool:
+                    pool.map(GetData, tuples)
 
-        # Get list of tuples to map in Pool
-        tuples = [(protein, mutation, path, threshold) for mutation in mutations]
+                # Arrays should be full by now, we save as csvs in folder in csv_path
+                # positions will be used as header
+                it.WriteCSV(prot_csv_path, nodes, positions, f"{protein}_{threshold}_nodes.csv")
+                it.WriteCSV(prot_csv_path, edges, positions, f"{protein}_{threshold}_edges.csv")
+                it.WriteCSV(prot_csv_path, weight, positions, f"{protein}_{threshold}_weight.csv")
+                it.WriteCSV(prot_csv_path, distance, positions, f"{protein}_{threshold}_distance.csv")
 
-        with mp.Pool(4) as pool:
-            pool.map(GetData, tuples)
-
-        # Arrays should be full by now, we save as csvs in csv_path
-        # positions will be used as header
-        it.WriteCSV(csv_path, nodes, positions, f"{protein}_{threshold}_nodes.csv")
-        it.WriteCSV(csv_path, edges, positions, f"{protein}_{threshold}_edges.csv")
-        it.WriteCSV(csv_path, weight, positions, f"{protein}_{threshold}_weight.csv")
-        it.WriteCSV(csv_path, distance, positions, f"{protein}_{threshold}_distance.csv")
+            # Save array from original data
+            original_data = np.vstack(
+                [thresholds, original_data])  # add thresholds
+            original_data = np.transpose(original_data)  # to add names as header
+            header = ['threshold', 'nodes', 'edges', 'weight', 'distance']
+            # Write CSV of original data
+            it.WriteCSV(prot_csv_path, original_data, header, f"{prot}_original.csv")
